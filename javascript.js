df285a746dfed981b02a3065d3c90e0abd834845
Youssef <ybelalmarzouk@gmail.com>
2024-02-03 02:37:29 +0200

    style:add advice box content

--- javascript.js
+++ javascript.js
@@ -1,13 +1,24 @@
-var data = "";
-
 async function get() {
 	const response = await fetch("https://api.adviceslip.com/advice");
-	var data = await response.json();
-	console.log(data);
-	var adviceBox = document.querySelector(".box-container");
-	adviceBox.innerHTML =
-		`
-	<div>${data.slip.advice}</div>
-	`
+	data = await response.json();
+	inputAdvice(data)
 }
+
+function inputAdvice(data) {
+	var adviceArea = document.querySelector(".advice-box-container");
+	adviceArea.innerHTML =
+		`
+			<div class="advice-ID">ADVICE #${data.slip.id}</div>
+			<div class="advice-area">
+				<div class="advice">"${data.slip.advice}"</div>
+				<div class="btn-area">
+					<button onclick="get()">click</button>
+				</div>
+			</div>
+		`
+}
+
+get()
+
+
 
